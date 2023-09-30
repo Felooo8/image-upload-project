6c0e447821b1a9dbc1cd51eb18f499dcf806b92f
Feliks <felobanka@gmail.com>
2023-09-30 22:47:13 +0200

    Init models

--- image_upload/admin.py
+++ image_upload/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from .models import Account, Tier
 
 # Register your models here.
+
+class AccountInline(admin.StackedInline):
+    model = Account
+    can_delete = False
+    verbose_name_plural = 'account'
+
+class UserAdmin(admin.ModelAdmin):
+    inlines = (AccountInline,)
+
+class TierAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Tier, TierAdmin)
--- image_upload/models.py
+++ image_upload/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import validate_comma_separated_integer_list
 
 # Create your models here.
+
+class Tier(models.Model):
+    name = models.CharField(max_length=100)
+    thumbnail_sizes = models.CharField(validators=[validate_comma_separated_integer_list], max_length=255)   
+    link_to_original = models.BooleanField(default=False)
+    expiring_link = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class Account(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    tier = models.ForeignKey(Tier, on_delete=models.SET_NULL, null=True, blank=True)
